7028bc977c35b5283c33d2e391f100d149ca2435
= <devop.jas@gmail.com>
2024-12-23 10:59:57 +0800

    learning how to develop a plugin

--- wp-content/plugins/first-unique-plugin/first-unique-plugin.php
+++ wp-content/plugins/first-unique-plugin/first-unique-plugin.php
@@ -2,31 +2,46 @@
 
 /*
   Plugin Name: Our Test Plugin
-  Description: A truly amazing plugin
+  Description: A truy amazing plugin
   Version: 1.0
-  Author: DevJas
-  Author URI: <https:youtube class="com">
+  Author: Brad
+  Author URI: https://www.linkedin.com/in/jason-evaristo-0a872b279/
 */
 
-add_filter('the_content', 'addToEndOfPost');
+
+class WordCountAndTimePlugin {
+  function __construct(){   
+    add_action('admin_menu', array($this, 'adminPage'));
+    add_action('admin_init', array($this,'settings'));
+   }
+  
+  function settings(){
+    add_settings_field(1,2,3,4,5);
+    //
+    register_setting('wordcountplugin','wcp_location',
+    array('sanitize_callback' => 
+    'sanitize_text_field', 'default' => '0'
+  ));
+  }
 
 
-/*
- this will che check if the post is single ]
- and then it is coming from the main routes using 
- the is_main_query method from wordpress
-*/
-function addToEndOfPost($content){
-    if(is_single() && is_main_query() ){
-      return  $content. "<p> My name is EJ <p/>";
-    }
-      return $content. "<p>did not satisfied condition</p>";
+  function adminPage(){
+    add_options_page('Word Count Settings','Word Count','manage_options',"Words Counter hehe",array($this, 'pageHtml'));
+  }
+
+  function pageHtml(){ ?>
+  <div class="wrap">
+    <h1>Word Count Settings</h1>
+  </div>
+
+  <?php
+
+  }
+   
+
 }
 
-
-
-
-
+$wordCounAndTimePlugin = new WordCountAndTimePlugin(); //creating an object/instance of a class
 
 
 
